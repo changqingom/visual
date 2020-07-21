536e4cbb2a3c7cd36b69f0ffaebdb55f9cb976e7
changqing <fancq@mti-sh.cn>
2020-07-21 21:43:03 +0800

    button

---
+++ src/components/Button.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <button
+      ref="button"
+      :style="{'background-color': backColor}"
+      :class="'button '+buttonSizeName"
+      @click="buttonClick"
+    >{{text}}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  data() {
+    return {};
+  },
+  props: {
+    size: {
+      type: String,
+      // "medium / small / mini"
+      default: "medium"
+    },
+    color: {
+      type: String,
+      default: "blue"
+    },
+    text: {
+      type: String,
+      default: "button"
+    }
+  },
+  computed: {
+    backColor() {
+      return this.color;
+    },
+    buttonSizeName() {
+      return "button-size-" + this.size;
+    }
+  },
+  methods: {
+    buttonClick(e) {
+      this.$emit("click", e);
+    }
+  }
+};
+</script>
+
+<style>
+.button {
+  color: #fff;
+}
+.button-size-medium {
+  height: 40px;
+  width: 100px;
+}
+.button-size-small {
+  height: 30px;
+  width: 75px;
+}
+.button-size-mini {
+  height: 20px;
+  width: 50px;
+}
+</style>
